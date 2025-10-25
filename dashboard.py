e43b769687572bd17a1070451970817c72e5abf1
istizara <132885688+istizara@users.noreply.github.com>
2025-10-25 16:46:19 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,40 @@ import streamlit as st
 # Menu dan Navigasi
 # ==========================
 
+# --- CUSTOM BACKGROUND COLOR (apply to all pages) ---
+page_bg = """
+<style>
+    /* Ubah warna background seluruh halaman */
+    [data-testid="stAppViewContainer"] {
+        background-color: #cfe1b9; 
+    }
+
+    /* Sidebar juga pakai warna yang sama */
+    [data-testid="stSidebar"] {
+        background-color: #98a77c; /* sedikit lebih gelap biar kontras */
+    }
+
+    /* Card atau container tetap putih */
+    [data-testid="stHeader"], .st-emotion-cache-1r6slb0 {
+        background: none;
+    }
+
+    /* Ubah warna tombol biar serasi */
+    .stButton>button {
+        background-color: #6da544;
+        color: white;
+        border-radius: 10px;
+        border: none;
+    }
+
+    .stButton>button:hover {
+        background-color: #588a34;
+        color: #fff;
+    }
+</style>
+"""
+st.markdown(page_bg, unsafe_allow_html=True)
+
 # --- SHARED ON ALL PAGES ---
 st.logo("Logo.png")
 
