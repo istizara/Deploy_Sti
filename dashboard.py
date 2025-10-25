0ad8f4531c7a4b9101dadae2699f45743b206c66
istizara <132885688+istizara@users.noreply.github.com>
2025-10-25 17:01:29 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,66 +7,81 @@ import streamlit as st
 # --- CUSTOM BACKGROUND COLOR (apply to all pages) ---
 page_bg = """
 <style>
-/* Warna latar seluruh halaman */
-[data-testid="stAppViewContainer"] {
-    background-color: #cfe1b9;
-}
+    /* Ubah warna background seluruh halaman */
+    [data-testid="stAppViewContainer"] {
+        background-color: #cfe1b9; 
+    }
 
-/* Warna latar area konten utama */
-[data-testid="stAppViewContainer"] > .main {
-    background-color: #cfe1b9;
-}
+    /* Sidebar juga pakai warna yang sama */
+    [data-testid="stSidebar"] {
+        background-color: #98a77c; 
+    }
 
-/* Warna latar area header di atas */
-[data-testid="stHeader"] {
-    background-color: #cfe1b9;
-    box-shadow: none; /* hilangkan garis bawah bayangan */
-}
+    /* Saat fitur dipilih (aktif) */
+    [data-testid="stSidebarNav"] a[aria-current="page"] {
+        background-color: #ffffff !important; /* putih */
+        color: #1b3d1b !important;
+        border-radius: 10px;
+        font-weight: 600;
+    }
+    
+    /* Hover effect biar interaktif */
+    [data-testid="stSidebarNav"] a:hover {
+        background-color: #dbe8c0 !important;
+        color: #1b3d1b !important;
+    }
 
-/* Warna latar kontainer isi */
-.block-container {
-    background-color: #cfe1b9;
-}
+    /* Ubah warna tombol biar serasi */
+    .stButton>button {
+        background-color: #6da544;
+        color: white;
+        border-radius: 10px;
+        border: none;
+    }
 
-/* Sidebar warna lembut */
-[data-testid="stSidebar"] {
-    background-color: #98a77c;
-}
-
-/* Warna teks sidebar agar lebih kontras */
-[data-testid="stSidebar"] * {
-    color: #0c0c0f !important; 
-    font-weight: 500;
-}
-
-/* Saat fitur dipilih (aktif) */
-[data-testid="stSidebarNav"] a[aria-current="page"] {
-    background-color: #ffffff !important; /* putih */
-    color: #1b3d1b !important;
-    border-radius: 10px;
-    font-weight: 600;
-}
-
-/* Hover effect biar interaktif */
-[data-testid="stSidebarNav"] a:hover {
-    background-color: #dbe8c0 !important;
-    color: #1b3d1b !important;
-}
-
-/* Tombol serasi dengan tema */
-.stButton>button {
-    background-color: #6da544;
-    color: white;
-    border-radius: 10px;
-    border: none;
-}
-
-.stButton>button:hover {
-    background-color: #588a34;
-    color: #fff;
-}
+    .stButton>button:hover {
+        background-color: #588a34;
+        color: #fff;
+    }
 </style>
 """
+st.markdown(page_bg, unsafe_allow_html=True)
+
+# --- SHARED ON ALL PAGES ---
+st.logo("Logo.png")
+
+# --- PAGE SETUP ---
+Homepage = st.Page(
+    "Homepage.py",
+    title="Homepage",
+    icon=":material/account_circle:",
+    default=True,
+)
+Object_detection_page = st.Page(
+    "Deteksi_Gambar.py",
+    title="Deteksi Gambar",
+    icon=":material/search:",
+)
+Klassification_page = st.Page(
+    "Klasifikasi_Gambar.py",
+    title="Klasifikasi Gambar",
+    icon=":material/eco:",
+)
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Info": [Homepage],
+         "Projects": [Object_detection_page, Klassification_page]
+    }
+)
+
+# --- SHARED ON ALL PAGES ---
+st.sidebar.markdown("Made with ❤️ by [Stiwww]")
+
+# --- RUN NAVIGATION ---
+pg.run()
+
 
 # --- SHARED ON ALL PAGES ---
 st.logo("Logo.png")
